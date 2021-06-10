e1819099e16d0e19747fcc858beb8926008f1f82
FinT <arsenijs.ovcinkins@gmail.com>
2021-06-10 08:23:39 +0300

    With reader from txt

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Reader {
     public static String teams() throws FileNotFoundException{
         String team;
-        File file = new File("C:/Users/ars/IdeaProjects/fileReader/src/teams.txt");
+        File file = new File("C:/Users/ars/IdeaProjects/TelegaBot/src/teams.txt");
         Scanner sc = new Scanner(file);
 
         // we just need to use \\Z as delimiter
